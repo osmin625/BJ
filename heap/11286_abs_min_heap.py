28fd76348d07b19a93a95484ef3e24feab1c3af0
osmin625 <tmdals179@gmail.com>
2022-08-17 16:56:42 +0900

    11286 go

--- heap/11286_abs_min_heap.py
+++ heap/11286_abs_min_heap.py
@@ -1,40 +1,44 @@
 import sys
 input = sys.stdin.readline
-
 h = [0]
+
 def insert(x):
-    h.append((abs(x),x))
-    idx = len(h) -1
+    h.append((abs(x), x))
+    idx = len(h) - 1
     while idx != 1:
-        if h[idx//2][0] > h[idx][0]:
-            h[idx//2],h[idx] = h[idx],h[idx//2]
-        elif h[idx//2][0] == h[idx][0] and h[idx//2][1] > h[idx][1]:
-            h[idx//2],h[idx] = h[idx],h[idx//2]
+        if h[idx // 2][0] > h[idx][0]:
+            h[idx // 2], h[idx] = h[idx], h[idx // 2]
+        elif h[idx // 2][0] == h[idx][0] and h[idx // 2][1] > h[idx][1]:
+            h[idx // 2], h[idx] = h[idx], h[idx // 2]
         idx //= 2
+
+
 def pop(x):
     if len(h) == 1:
         print(0)
         return
-    h[1], h[-1] = h[-1],h[1]
+    h[1], h[-1] = h[-1], h[1]
     print(h.pop()[1])
     i = 1
     while True:
         if len(h) > 2 * i + 1:
-            t = sorted([h[i],h[2 * i],h[2 * i + 1]],key = lambda x:(x[0], x[1]))
+            t = sorted([h[i], h[2 * i], h[2 * i + 1]], key=lambda x: (x[0], x[1]))
             if t[0] == h[2 * i]:
-                h[i], h[2 * i] = h[2 * i],h[i]
+                h[i], h[2 * i] = h[2 * i], h[i]
                 i *= 2
             elif t[0] == h[2 * i + 1]:
-                h[i], h[2 * i + 1] = h[2 * i + 1],h[i]
+                h[i], h[2 * i + 1] = h[2 * i + 1], h[i]
                 i = i * 2 + 1
             else:
                 break
         elif len(h) > 2 * i:
-            t = sorted([h[i],h[2 * i]],key = lambda x:(x[0], x[1]))
+            t = sorted([h[i], h[2 * i]], key=lambda x: (x[0], x[1]))
             if t[0] == h[2 * i]:
-                h[i], h[2 * i] = h[2 * i],h[i]
+                h[i], h[2 * i] = h[2 * i], h[i]
+            break
         else:
             break
+
 N = int(input())
 for _ in range(N):
     x = int(input())
